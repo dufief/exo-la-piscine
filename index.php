2c82d6abe1f1a2c8c2671fd1c645fc75250e2f00
lapiscine <thomas.dufief@lapiscine.pro>
2018-03-02 16:14:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Mon exo github</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<h1>Exo GitHub</h1>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+
+</body>
+</html>
+  
