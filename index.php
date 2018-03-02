44006cdba1cfb2eedfdede425e8e27a15a95500f
lapiscine <thomas.dufief@lapiscine.pro>
2018-03-02 16:39:52 +0100

    Create h2

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<title>Mon exo github</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
 <body>
 
 	<h1>Exo GitHub</h1>
+
+	<h2>htghfdglj</h2>
 
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
